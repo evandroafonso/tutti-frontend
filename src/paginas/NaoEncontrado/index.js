fbb6f55fbd307da03633d29863e1ced7a7893761
Evandro Afonso Rodrigues <evandroafonsorodrigues@gmail.com>
2023-04-23 18:47:28 -0300

    add construtor

--- src/paginas/NaoEncontrado/index.js
+++ src/paginas/NaoEncontrado/index.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class NaoEncontrado extends Component {
-
+  constructor(props) {
+      super(props);
+    }
+    
   render() {
     return (
       <section className="background">
