a16b6962b5cce308cbb167ae421a35bd8a4be6fa
Evandro Afonso Rodrigues <evandroafonsorodrigues@gmail.com>
2025-03-29 22:01:17 -0300

    adiciona user.js

---
+++ src/store/user.js
@@ -0,0 +1,19 @@
+import {defineStore} from "pinia";
+import axiosClient from "../axios.js";
+
+const useUserStore = defineStore('user', {
+  state: () => ({
+    user: null
+  }),
+  actions: {
+    fetchUser() {
+      return axiosClient.get('/users')
+        .then(({data}) => {
+          console.log(data)
+          this.user = data
+        })
+    }
+  }
+})
+
+export default useUserStore;
