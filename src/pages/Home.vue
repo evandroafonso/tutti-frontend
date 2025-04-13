fbaf8b89df978046758c68217570d230cae73aa7
evandroafonso <evandroafonsorodrigues@gmail.com>
2025-04-12 22:46:16 -0300

    adiciona landing page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,17 @@
 <template>
     <div class="bg-white">
+      
       <header class="absolute inset-x-0 top-0 z-50">
         <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div class="flex lg:flex-1">
             <a href="#" class="-m-1.5 p-1.5">
-              <span class="sr-only">Your Company</span>
-              <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+              <span class="sr-only">Tutti</span>
+              <img class="h-14 w-auto" src="/music.svg" alt=""/>
+              <h1 class="text-2xl font-bold text-gray-700">Tutti</h1>
             </a>
           </div>
-          <div class="flex lg:hidden">
-            <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-              <span class="sr-only">Open main menu</span>
-              <Bars3Icon class="size-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div class="hidden lg:flex lg:gap-x-12">
+          <div class="hidden lg:flex lg:gap-x-8">
             <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm/6 font-semibold text-gray-900">{{ item.name }}</a>
-          </div>
-          <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" class="text-sm/6 font-semibold text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
           </div>
         </nav>
         <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
@@ -34,16 +27,6 @@
                 <XMarkIcon class="size-6" aria-hidden="true" />
               </button>
             </div>
-            <div class="mt-6 flow-root">
-              <div class="-my-6 divide-y divide-gray-500/10">
-                <div class="space-y-2 py-6">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">{{ item.name }}</a>
-                </div>
-                <div class="py-6">
-                  <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
-                </div>
-              </div>
-            </div>
           </DialogPanel>
         </Dialog>
       </header>
@@ -52,18 +35,17 @@
         <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
           <div class="relative left-[calc(50%-11rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
         </div>
-        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div class="mx-auto max-w-2xl py-32 sm:py-30 lg:py-20">
           <div class="hidden sm:mb-8 sm:flex sm:justify-center">
             <div class="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding. <a href="#" class="font-semibold text-indigo-600"><span class="absolute inset-0" aria-hidden="true" />Read more <span aria-hidden="true">&rarr;</span></a>
+              Saiba mais sobre nossa missão <a href="#" class="font-semibold text-green-600"><span class="absolute inset-0" aria-hidden="true" />aqui <span aria-hidden="true">&rarr;</span></a>
             </div>
           </div>
           <div class="text-center">
-            <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">Data to enrich your online business</h1>
-            <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat.</p>
+            <h1 class="text-5xl font-bold tracking-tight text-gray-800 sm:text-6xl">Musicalização Infantil a seu alcance!</h1>
+            <p class="mt-8 text-lg font-bold text-green-600 sm:text-xl/8">Aprenda a transformar sons e brincadeiras em momentos mágicos de aprendizagem </p>
             <div class="mt-10 flex items-center justify-center gap-x-6">
-              <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
-              <a href="#" class="text-sm/6 font-semibold text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+              <a href="#" class="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-green-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">Get started</a>
             </div>
           </div>
         </div>
@@ -77,8 +59,8 @@
       <div class="mx-auto aspect-1155/678 w-[72.1875rem] bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" />
     </div>
     <div class="mx-auto max-w-4xl text-center">
-      <h2 class="text-base/7 font-semibold text-indigo-600">Pricing</h2>
-      <p class="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">Choose the right plan for you</p>
+      <h2 class="text-base/7 font-semibold text-green-600">Planos e Preços</h2>
+      <p class="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-6xl">Escolha o plano certo para voçê</p>
     </div>
     <p class="mx-auto mt-6 max-w-2xl text-center text-lg font-medium text-pretty text-gray-600 sm:text-xl/8">Choose an affordable plan that’s packed with the best features for engaging your audience, creating customer loyalty, and driving sales.</p>
     <div class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2">
@@ -99,6 +81,11 @@
       </div>
     </div>
   </div>
+  <div class="fixed bottom-4 right-4 z-50">
+  <img src="/passaro-guitar.svg"  alt="Imagem fixa"
+  class="fixed bottom-4 right-4 h-20 w-20 cursor-pointer hover:animate-bounce"
+/>
+</div>
   
 
 <footer class="bg-white dark:bg-gray-900">
@@ -195,10 +182,10 @@
   import { CheckIcon } from '@heroicons/vue/20/solid'
   
   const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
+    { name: 'Sobre nós', href: '#' },
+    { name: 'Preços', href: '#' },
+    { name: 'Cadastre-se', href: '#' },
+    { name: 'Login', href: '#' },
   ]
   
   const mobileMenuOpen = ref(false)
