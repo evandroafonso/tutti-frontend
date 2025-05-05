93efe19c9f30b6138528a0e047054be8d57752c5
Evandro Afonso Rodrigues <evandroafonsorodrigues@gmail.com>
2025-05-05 06:54:07 -0300

    corrige erro em botões

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,7 +1,16 @@
 <template>
   <button
     :disabled="disabled"
-    :class="['px-4 py-2 font-bold text-white rounded focus:outline-none', disabled ? 'bg-gray-400 cursor-not-allowed' : `bg-${color}-600 hover:bg-${color}-700`]"
+    :class="[
+      'px-4 py-2 font-bold text-white rounded focus:outline-none',
+      disabled
+        ? 'bg-gray-400 cursor-not-allowed'
+        : {
+            'bg-green-600 hover:bg-green-700': color === 'green',
+            'bg-blue-600 hover:bg-blue-700': color === 'blue',
+            'bg-red-600 hover:bg-red-700': color === 'red',
+          },
+    ]"
     @click="$emit('click')"
   >
     {{ text }}
